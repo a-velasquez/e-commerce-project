b31235d2ca67ea025956a9d174602735885c394b
Alex Velazquez <avelasquez075@gmail.com>
2021-06-21 14:57:22 -0700

    add routes for about, cart, produtcs, singleProduct, error

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ function App() {
 				<Route exact path='/'>
 					<Home />
 				</Route>
+				<Route exact path='/about'>
+					<About />
+				</Route>
+				<Route exact path='/cart'>
+					<Cart />
+				</Route>
+				<Route exact path='/products'>
+					<Products />
+				</Route>
+				<Route exact path='/products/:id' children={<SingleProduct />} />
+				<Route exact path='/checkout'>
+					<Checkout />
+				</Route>
+				<Route path='*'>
+					<Error />
+				</Route>
 			</Switch>
 		</Router>
 	)
