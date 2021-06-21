671fc47e2af219040922ad05e4f5dd0e030c0974
Alex Velazquez <avelasquez075@gmail.com>
2021-06-21 14:48:40 -0700

    import pages from pages/index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {Navbar, Sidebar, Footer} from './components'
+import {
+	Home,
+	Products,
+	SingleProduct,
+	About,
+	Cart,
+	Checkout,
+	Error,
+	PrivateRoute
+} from './pages'
 
 function App() {
-	return <h4>e-commerce setup</h4>
+	return (
+		<div>
+			<h4>e-shop starter</h4>
+		</div>
+	)
 }
 
 export default App
