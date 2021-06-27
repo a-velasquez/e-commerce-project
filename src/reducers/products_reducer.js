8e33ae4cea62ffdc6401f4030173d3fdf9f6243d
Alex Velazquez <avelasquez075@gmail.com>
2021-06-26 23:42:03 -0700

    add GET_PRODUCTS_SUCCESS reducer

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -19,6 +19,17 @@ const products_reducer = (state, action) => {
 	if (action.type === GET_PRODUCTS_BEGIN) {
 		return {...state, products_loading: true}
 	}
+	if (action.type === GET_PRODUCTS_SUCCESS) {
+		const featured_products = action.payload.filter(
+			(product) => product.featured === true
+		)
+		return {
+			...state,
+			products_loading: false,
+			products: action.payload,
+			featured_products
+		}
+	}
 	throw new Error(`No Matching "${action.type}" - action type`)
 }
 
