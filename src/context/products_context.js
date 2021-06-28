2e747aad0c3c22879cdd1258cc0443aacaa04528
Alex Velazquez <avelasquez075@gmail.com>
2021-06-27 18:25:36 -0700

    add properties for initialState for single_product

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -19,7 +19,10 @@ const initialState = {
 	products_loading: false,
 	products_error: false,
 	products: [],
-	featured_products: []
+	featured_products: [],
+	single_product_loading: false,
+	single_product_error: false,
+	single_product: {}
 }
 
 const ProductsContext = React.createContext()
