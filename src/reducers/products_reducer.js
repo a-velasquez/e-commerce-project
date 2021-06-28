1372f273f5983a3a5299cf7e7ded3ec162d9b428
Alex Velazquez <avelasquez075@gmail.com>
2021-06-27 23:11:11 -0700

    add GET_SINGLE_PRODUCT_ERROR reducer

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -43,6 +43,13 @@ const products_reducer = (state, action) => {
 			single_product: action.payload
 		}
 	}
+	if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+		return {
+			...state,
+			single_product_loading: false,
+			single_product_error: true
+		}
+	}
 	throw new Error(`No Matching "${action.type}" - action type`)
 }
 
