7a058cc49829dae51e5f04cf1abe7b3bd92dd23c
Alex Velazquez <avelasquez075@gmail.com>
2021-06-28 21:46:15 -0700

    add if (loading) and if (error)

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -27,6 +27,13 @@ const SingleProductPage = () => {
 		fetchSingleProduct(`${url}${id}`)
 	}, [id])
 	console.log(product)
+
+	if (loading) {
+		return <Loading />
+	}
+	if (error) {
+		return <Error />
+	}
 	return <h4>single product page</h4>
 }
 
