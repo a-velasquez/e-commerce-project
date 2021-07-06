6e309a55f20f8cd8f4e24c2545eaf5af0f0a02b1
Alex Velazquez <avelasquez075@gmail.com>
2021-07-06 14:26:34 -0700

    add <select> for company names

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -63,6 +63,20 @@ const Filters = () => {
 						</div>
 					</div>
 					{/* end of categories */}
+					{/* company */}
+					<div className='form-control'>
+						<h5>company</h5>
+						<select
+							name='company'
+							value={company}
+							onChange={updateFilters}
+							className='company'>
+							{companies.map((c, index) => {
+								return <option key={index}>{c}</option>
+							})}
+						</select>
+					</div>
+					{/* end of company */}
 				</form>
 			</div>
 		</Wrapper>
