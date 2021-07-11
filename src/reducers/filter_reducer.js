b4f30bbe00018f8ec9a03f54741fa703a1265427
Alex Velazquez <avelasquez075@gmail.com>
2021-07-11 13:10:48 -0700

    grab all_products from state, grab state.filters, let tempProducts

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -75,7 +75,12 @@ const filter_reducer = (state, action) => {
 		return { ...state, filters: { ...state.filters, [name]: value } }
 	}
 	if (action.type === FILTER_PRODUCTS) {
-		return { ...state }
+		const { all_products } = state
+		const { text, category, company, color, price, shipping } = state.filters
+		console.log(state.filters)
+		let tempProducts = [...all_products]
+		// filtering
+		return { ...state, filtered_products: tempProducts }
 	}
 	if (action.type === CLEAR_FILTERS) {
 		return {
