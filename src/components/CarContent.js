50e954cd7292a6ef16a9921e2005c436adc6b667
Alex Velazquez <avelasquez075@gmail.com>
2021-07-12 13:30:04 -0700

    add link to products and button to clear cart

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -15,6 +15,18 @@ const CartContent = () => {
 			{cart.map((item) => {
 				return <CartItem key={item.id} {...item} />
 			})}
+			<hr />
+			<div className='link-container'>
+				<Link to='/products' className='link-btn'>
+					continue shopping
+				</Link>
+				<button
+					type='button'
+					className='link-btn clear-btn'
+					onClick={clearCart}>
+					clear cart
+				</button>
+			</div>
 		</Wrapper>
 	)
 }
