97275536956f03a116d90ca342579722d5c94d4c
Alex Velazquez <avelasquez075@gmail.com>
2021-07-15 14:35:08 -0700

    add AuthWrapper around Routes

--- src/App.js
+++ src/App.js
@@ -9,37 +9,43 @@ import {
 	Cart,
 	Checkout,
 	Error,
-	Private
+	Private,
+	AuthWrapper
 } from './pages'
 
 function App() {
 	return (
-		<Router>
-			<Navbar />
-			<Sidebar />
-			<Switch>
-				<Route exact path='/'>
-					<Home />
-				</Route>
-				<Route exact path='/about'>
-					<About />
-				</Route>
-				<Route exact path='/cart'>
-					<Cart />
-				</Route>
-				<Route exact path='/products'>
-					<Products />
-				</Route>
-				<Route exact path='/products/:id' children={<SingleProduct />}></Route>
-				<Private exact path='/checkout'>
-					<Checkout />
-				</Private>
-				<Route path='*'>
-					<Error />
-				</Route>
-			</Switch>
-			<Footer />
-		</Router>
+		<AuthWrapper>
+			<Router>
+				<Navbar />
+				<Sidebar />
+				<Switch>
+					<Route exact path='/'>
+						<Home />
+					</Route>
+					<Route exact path='/about'>
+						<About />
+					</Route>
+					<Route exact path='/cart'>
+						<Cart />
+					</Route>
+					<Route exact path='/products'>
+						<Products />
+					</Route>
+					<Route
+						exact
+						path='/products/:id'
+						children={<SingleProduct />}></Route>
+					<Private exact path='/checkout'>
+						<Checkout />
+					</Private>
+					<Route path='*'>
+						<Error />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
+		</AuthWrapper>
 	)
 }
 
