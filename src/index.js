eaff5d50630c4fa712ba3e9d8b3e22e4f0656fa8
Alex Velazquez <avelasquez075@gmail.com>
2021-07-14 22:44:33 -0700

    wrap App in AuthProvider

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
-	<ProductsProvider>
-		<FilterProvider>
-			<CartProvider>
-				<App />
-			</CartProvider>
-		</FilterProvider>
-	</ProductsProvider>,
+	<Auth0Provider
+		domain='dev-8z5t2mgw.us.auth0.com'
+		clientId='PZcV16dLb5FJQXburiq1bzEvYsYOCx1U'
+		redirectUri={window.location.origin}>
+		<ProductsProvider>
+			<FilterProvider>
+				<CartProvider>
+					<App />
+				</CartProvider>
+			</FilterProvider>
+		</ProductsProvider>
+	</Auth0Provider>,
 	document.getElementById('root')
 )
