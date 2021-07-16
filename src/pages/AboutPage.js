1ad2fb06be8d3566f3fc210a91caf292b44086a7
Alex Velazquez <avelasquez075@gmail.com>
2021-07-16 13:53:56 -0700

    fixed alt description to fix console warning

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import {PageHero} from '../components'
+import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
@@ -8,7 +8,7 @@ const AboutPage = () => {
 		<main>
 			<PageHero title='about' />
 			<Wrapper className='page section section-center'>
-				<img src={aboutImg} alt='desk image' />
+				<img src={aboutImg} alt='desk' />
 				<article>
 					<div className='title'>
 						<h2>our story</h2>
