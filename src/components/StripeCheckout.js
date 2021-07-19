6405ecabbcac6ea9c5454872fd1566f123a33e5e
Alex Velazquez <avelasquez075@gmail.com>
2021-07-19 13:28:45 -0700

    finish handleSubmit() and handleChange() using Stripe boilerplate

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,6 +12,7 @@ import { useCartContext } from '../context/cart_context'
 import { useUserContext } from '../context/user_context'
 import { formatPrice } from '../utils/helpers'
 import { useHistory } from 'react-router-dom'
+import { getElementError } from '@testing-library/react'
 
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
 
@@ -61,9 +62,32 @@ const CheckoutForm = () => {
 		// eslint-disable-next-line
 	}, [])
 
-	const handleChange = async (event) => {}
+	const handleChange = async (event) => {
+		setDisabled(event.empty)
+		setError(event.error ? event.error.message : '')
+	}
 
-	const handleSubmit = async (ev) => {}
+	const handleSubmit = async (ev) => {
+		ev.preventDefault()
+		setProcessing(true)
+		const payload = await stripe.confirmCardPayment(clientSecret, {
+			payment_method: {
+				card: elements.getElement(CardElement)
+			}
+		})
+		if (payload.error) {
+			setError(`Payment failed ${payload.error.message}`)
+			setProcessing(false)
+		} else {
+			setError(null)
+			setProcessing(false)
+			setSucceeded(true)
+			setTimeout(() => {
+				clearCart()
+				history.push('/')
+			}, 10000)
+		}
+	}
 
 	return (
 		<div>
