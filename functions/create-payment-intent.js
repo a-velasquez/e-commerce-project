a430a95f5734309bde21edd4db7226b6d75684b8
Alex Velazquez <avelasquez075@gmail.com>
2021-07-19 13:12:38 -0700

    add try/catch with paymentIntent, send Stripe client secret

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -3,10 +3,31 @@ require('dotenv').config()
 const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY)
 
 exports.handler = async function (event, context) {
-	const { cart, shipping, total_amount } = JSON.parse(event.body)
-	console.log(event)
+	if (event.body) {
+		const { cart, shipping, total_amount } = JSON.parse(event.body)
+
+		const calculateOrderAmount = () => {
+			return shipping + total_amount
+		}
+		try {
+			const paymentIntent = await stripe.paymentIntents.create({
+				amount: calculateOrderAmount(),
+				currency: 'usd'
+			})
+			return {
+				statusCode: 200,
+				body: JSON.stringify({ clientSecret: paymentIntent.client_secret })
+			}
+		} catch (error) {
+			return {
+				statusCode: 500,
+				body: JSON.stringify({ msg: error.message })
+			}
+		}
+	}
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify(cart)
+		body: 'Create Payment Intent'
 	}
 }
